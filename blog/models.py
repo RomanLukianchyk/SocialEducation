222296d4f64abbbc6b12549788ea7e71a26466d4
RomanLukianchyk <19arrow19@gmail.com>
2024-08-09 20:56:01 +0200

    Added email info, cloudinary revorked

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    image = models.ImageField(upload_to='media/posts/', null=True, blank=True)
+    image = CloudinaryField('image', null=True, blank=True)
     tags = models.CharField(max_length=100, blank=True, default='')
     created_at = models.DateTimeField(default=timezone.now)
 
